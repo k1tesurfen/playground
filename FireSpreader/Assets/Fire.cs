7675642f74e2b06220286437f5314eeb20261197
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2019-12-04 18:46:48 +0100

    init fire

---
+++ FireSpreader/Assets/Fire.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private float timer = 0.5f;
+
+    public Spark sparkObject;
+
+    private bool spreading = true;
+
+    public GameObject flames;
+
+    public void StartSpreading()
+    {
+        timer = 2f;
+        spreading = true;
+        flames.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spreading)
+        {
+            if (timer < 0f)
+            {
+                Spark spark = Instantiate(sparkObject, transform.position + 0.5f * Vector3.up, Quaternion.identity, transform.parent) as Spark;
+                spark.GetComponent<Rigidbody>().AddForce(300 * Vector3.up + Quaternion.Euler(Vector3.up * Random.Range(0, 359)) * (300 * Vector3.left));
+                spreading = false;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+
+
+        }
+    }
+}
---
+++ FireSpreader/Assets/Spark.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Spark : MonoBehaviour
+{
+    public Fire fireObject;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Fire fire = Instantiate(fireObject, transform.parent) as Fire;
+        fire.transform.position = transform.position;
+        fire.StartSpreading();
+
+        Destroy(gameObject);
+    }
+}
