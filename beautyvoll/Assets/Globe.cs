ae66f3057d6c975121f2bd4c7fd2af444d48b0dc
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2020-02-19 15:36:27 +0100

    init beautivoll

---
+++ beautyvoll/Assets/Globe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globe : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //transform.localRotation *= Quaternion.Euler()
+        }
+    }
+
+    public void Rotate(Vector3 eulerAngles)
+    {
+         
+    }
+}
